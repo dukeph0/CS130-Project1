0c1b14d5983c6910366ff863487156efac1ef497
dukeph0 <dpham5161@outlook.com>
2022-07-07 08:14:44 +0100

    Update render pixel function

--- proj-rt/render_world.cpp
+++ proj-rt/render_world.cpp
@@ -29,8 +29,13 @@ Hit Render_World::Closest_Intersection(const Ray& ray)
 // set up the initial view ray and call
 void Render_World::Render_Pixel(const ivec2& pixel_index)
 {
-    TODO; // set up the initial view ray here
+    //TODO; // set up the initial view ray here
     Ray ray;
+    // end_point is the camera position
+    ray.endpoint = camera.position;
+    // direction is a unit vectore from the camera position
+    // to the world position of the pixel.
+    ray.direction = (camera.World_Position(pixel_index) - ray.endpoint).normalized();
     vec3 color=Cast_Ray(ray,1);
     camera.Set_Pixel(pixel_index,Pixel_Color(color));
 }
